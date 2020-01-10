538b0c6b10778a46610787f63cb5ceb5d65cbce7
Brendan Hart <hartbren@gmail.com>
2020-01-10 11:00:39 +1030

    Complete sample page, and add CSS and a layout
    - also update readme with better setup instructions

--- README.md
+++ README.md
@@ -10,23 +10,32 @@ This project was developed on the following environment:
 - CentOS Linux release 7.7.1908
 - PHP 7.2.26
 - Extra PHP Modules: php-zip, php-mbstring, php-pecl-openssl 
+- Composer already available
 
 However, it should be executable on any recent Linux with PHP >=7.2 (and those extra modules).
 
 ## How to run
 
-After cloning the project from Github, change directory to 
-the \[install root\]/laravel-lumen-api-demo folder and execute:
+**To run and test on localhost:**
 
-    php -S localhost:8000 -t public
+1. Clone the project from Github
+1. Change directory to `<install root>/laravel-lumen-api-demo`
+1. Run `php composer.phar install` to pull down the dependencies
+1. Clone the `.env.exmaple` file to `.env` and configure a random "APP_KEY" value
+1. Run the builtin php server to serve the application on localhost:8000 
+    
+    `php -S localhost:8000 -t public`
 
-The API will be accessible on http://localhost:8000
+1. The API will be accessible on `http://localhost:8000/api/*`
+1. A page with some notes and  samples to access the endpoints is located at the site root 
 
-To access from an external system you'll need to either:
-- setup a "proper" webserver (details left an an exercise for the reader) 
+**To access "externally" you'll need to either:**
+- setup a "proper" webserver (details left an an exercise for the reader)
+ 
+OR
+
 - first ensure the relevant port (8000?) is configured on the firewall to allow for external access (Centos uses iptables), and then run the php built-in server with a command something like this:
 
-
-       php -S <server ip address>:8000 -t public
+  `php -S <server ip address>:8000 -t public`
 
 
--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -1,45 +1,213 @@
-<html>
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Laravel Lumen API Demo</title>
+    <meta name="description" content="Laravel Lumen API Demo">
+    <meta name="author" content="Brendan Hart">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/pure.css">
+
+    <style>
+        .content-wrapper {
+            display: grid;
+            grid-template-columns: 1fr 900px 1fr;
+        }
+
+        .form-input-wrapper {
+            margin-bottom: 1em;
+        }
+
+        li {
+            margin-bottom: 1em;
+        }
+
+        .output {
+            min-width: 600px;
+            min-height: 100px;
+            padding: 15px;
+            border: 1px solid black;
+        }
+    </style>
+
+</head>
+
 <body>
-<h1>Laravel Lumen API Demo</h1>
+<div class="content-wrapper">
+    <div></div>
+    <div>
+        <h1>Laravel Lumen API Demo</h1>
 
-<p>Welcome to the "Date Interval Calculator" API demo.</p>
-<p>Each section in the page below allows interaction with one of the endpoints described in the requirements
-    document.</p>
+        <p>
+            Welcome to the "Date Interval Calculator" API demo.
+        </p>
+        <p>
+            Each section in the page below allows interaction with one of the endpoints described in the requirements
+            document.
+        </p>
 
-<hr>
-<h2>Days between two datetimes</h2>
+        <h3>Notes:</h3>
+        <ul>
+            <li>
+                No authentication/authorisation, rate limiting or other API secondary requirements have been
+                implemented.
+            </li>
+            <li>
+                API endpoints accept both basic url-encoded HTML form and JSON formatted requests
+            </li>
+            <li>
+                The requirements were interpreted to mean that only "complete" time periods would be counted; for
+                example, a start datetime with a time component is rounded up to the start of the next day.
+            </li>
+            <li>
+                The requirements were interpreted to mean that only "complete" time periods between the endpoints would
+                be counted; for example, a start datetime with a time component is rounded up to the start of the next
+                day.
+            </li>
+            <li>
+                The "complete weeks" endpoint assumes the weeks to be counted begin on Mondays.
+            </li>
+        </ul>
 
-<form id="days-between-datetimes" name="days-between-datetimes" method="POST"
-      action="{{ route('days-between-dates')  }}">
-    <label for="startdate">Start Datetime</label>
-    <input type="text" id="startdate" name="startDateTime" value="">
+        <hr>
 
-    <label for="enddate">End Datetime</label>
-    <input type="text" id="enddate" name="endDateTime" value="">
+        <div>
+            <h2>Days between two datetimes</h2>
 
-    <label for="outputunit">Output Units (optional)</label>
+            <p>
+                Endpoint address: <em>/api/days-between-dates</em>
+            </p>
 
-    <select id="outputunit" name="outputUnit">
-        <option value="default" selected>Default (Decimal number of days)</option>
-        <option value="seconds">Seconds</option>
-        <option value="minutes">Minutes</option>
-        <option value="hours">Hours</option>
-        <option value="years">Years</option>
-    </select>
+            <form id="days-between-datetimes" name="days-between-datetimes" class="pure-form pure-form-stacked"
+                  method="POST"
+                  action="{{ route('days-between-dates')  }}">
+                <fieldset>
 
-    <input type="submit">
+                    <div class="form-input-wrapper">
+                        <label for="startdate">Start Datetime</label>
+                        <input type="text" id="startdate" name="startDateTime" value="">
+                    </div>
 
-</form>
+                    <div class="form-input-wrapper">
+                        <label for="enddate">End Datetime</label>
+                        <input type="text" id="enddate" name="endDateTime" value="">
+                    </div>
 
-<p>Output:</p>
-<div id="days-between-datetimes-output"></div>
+                    <div class="form-input-wrapper">
+                        <label for="outputunit">Output Units (optional)</label>
+                        <select id="outputunit" name="outputUnit">
+                            <option value="default" selected>Default (Decimal number of days)</option>
+                            <option value="seconds">Seconds</option>
+                            <option value="minutes">Minutes</option>
+                            <option value="hours">Hours</option>
+                            <option value="years">Years</option>
+                        </select>
+                    </div>
 
-<hr>
-<h2>Week Days between two datetimes</h2>
+                    <input type="submit" class="pure-button pure-button-primary">
+                </fieldset>
 
-<hr>
-<h2>Complete Weeks between two datetimes</h2>
+            </form>
 
+            <p>Output:</p>
+            <div id="days-between-datetimes-output" class="output"></div>
+        </div>
+
+        <hr>
+
+        <div>
+            <h2>Week Days between two datetimes</h2>
+
+            <p>Endpoint address: <em>/api/weekdays-between-dates</em></p>
+
+            <form id="weekdays-between-datetimes" name="weekdays-between-datetimes" class="pure-form pure-form-stacked"
+                  method="POST"
+                  action="{{ route('weekdays-between-dates')  }}">
+                <fieldset>
+
+                    <div class="form-input-wrapper">
+                        <label for="startDateTime">Start Datetime</label>
+                        <input type="text" id="startdate" name="startDateTime" value="">
+                    </div>
+
+                    <div class="form-input-wrapper">
+                        <label for="enddate">End Datetime</label>
+                        <input type="text" id="enddate" name="endDateTime" value="">
+                    </div>
+
+                    <div class="form-input-wrapper">
+                        <label for="outputunit">Output Units (optional)</label>
+                        <select id="outputunit" name="outputUnit">
+                            <option value="default" selected>Default (Decimal number of days)</option>
+                            <option value="seconds">Seconds</option>
+                            <option value="minutes">Minutes</option>
+                            <option value="hours">Hours</option>
+                            <option value="years">Years</option>
+                        </select>
+                    </div>
+
+                    <input type="submit" class="pure-button pure-button-primary">
+                </fieldset>
+
+            </form>
+
+            <p>Output:</p>
+            <div id="weekdays-between-datetimes-output" class="output"></div>
+
+        </div>
+
+        <hr>
+
+        <div>
+            <h2>Complete Weeks between two datetimes</h2>
+
+            <p>Endpoint address: <em>/api/complete-weeks-between-dates</em></p>
+
+            <form id="complete-weeks-between-datetimes" name="complete-weeks-between-datetimes"
+                  class="pure-form pure-form-stacked"
+                  method="POST"
+                  action="{{ route('complete-weeks-between-dates')  }}">
+                <fieldset>
+
+                    <div class="form-input-wrapper">
+                        <label for="startDateTime">Start Datetime</label>
+                        <input type="text" id="startdate" name="startDateTime" value="">
+                    </div>
+
+                    <div class="form-input-wrapper">
+                        <label for="enddate">End Datetime</label>
+                        <input type="text" id="enddate" name="endDateTime" value="">
+                    </div>
+
+                    <div class="form-input-wrapper">
+                        <label for="outputunit">Output Units (optional)</label>
+                        <select id="outputunit" name="outputUnit">
+                            <option value="default" selected>Default (Decimal number of weeks)</option>
+                            <option value="seconds">Seconds</option>
+                            <option value="minutes">Minutes</option>
+                            <option value="hours">Hours</option>
+                            <option value="years">Years</option>
+                        </select>
+                    </div>
+
+                    <input type="submit" class="pure-button pure-button-primary">
+                </fieldset>
+
+            </form>
+
+            <p>Output:</p>
+            <div id="complete-weeks-between-datetimes-output" class="output"></div>
+
+        </div>
+
+    </div>
+    <div></div>
+</div>
 
 <script>
 
@@ -59,13 +227,13 @@
             },
             body: JSON.stringify(formdata),
         })
-        .then((response) => response.json())
-        .then((data) => {
-            outputEl.innerText = JSON.stringify(data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+            .then((response) => response.json())
+            .then((data) => {
+                outputEl.innerText = JSON.stringify(data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     }
 
     let form_daysBetweenDatetimes = document.getElementById('days-between-datetimes');
@@ -74,6 +242,22 @@
         event.preventDefault();
         let outputEl = document.getElementById("days-between-datetimes-output");
         sendData(form_daysBetweenDatetimes, form_daysBetweenDatetimes.action, outputEl);
+    });
+
+    let form_weekdaysBetweenDatetimes = document.getElementById('weekdays-between-datetimes');
+
+    form_weekdaysBetweenDatetimes.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let outputEl = document.getElementById("weekdays-between-datetimes-output");
+        sendData(form_weekdaysBetweenDatetimes, form_weekdaysBetweenDatetimes.action, outputEl);
+    });
+
+    let form_completeWeeksBetweenDatetimes = document.getElementById('complete-weeks-between-datetimes');
+
+    form_completeWeeksBetweenDatetimes.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let outputEl = document.getElementById("complete-weeks-between-datetimes-output");
+        sendData(form_completeWeeksBetweenDatetimes, form_completeWeeksBetweenDatetimes.action, outputEl);
     });
 </script>
 
